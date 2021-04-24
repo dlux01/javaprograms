e80b24dd9543ed6fb7d75028f7a448ef2287cd30
dlux01 <losemu45@gmail.com>
2021-04-24 13:54:39 +0100

    second commit

--- sasoworld.java
+++ sasoworld.java
@@ -1,6 +1,11 @@
-public class sasoworld {
+ public class sasoworld {
 
 	public static void main(string[] args) {// prints "saso world" to the terminal window.
 		system.out.println("saso world");
 	}
 }
+  	public static void {
+   				main(string) {// prints "saso world 2" to the terminal window.
+			system.out.println("saso world 2");
+	}
+} 
